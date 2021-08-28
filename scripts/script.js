d9cd0878968f0b817267e17970c3e933b3530ccf
Daniel Castano <daniel.zapata.682@gmail.com>
2021-08-27 19:24:06 -0500

    Finished localStorage data upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,68 @@
-// IMC = Peso (kg) / altura (m)2
+class imcEntry {
+  constructor(gender, age, weight, height, imc, idealWeight) {
+    this.gender = gender;
+    this.age = age;
+    this.weight = weight;
+    this.height = height;
+    this.imc = imc;
+    this.idealWeight = idealWeight;
+  }
+}
+
+const resetForm = () => {
+  const $gender = document.querySelectorAll(".radio-button");
+  const $textData = document.querySelectorAll(".text-box");
+  $gender.forEach((i) => {
+    i.checked = false;
+  });
+  $textData.forEach((i) => {
+    i.value = "";
+  });
+};
+
 const calcImc = () => {
-  const $sex = document.querySelectorAll(".radio-button");
+  const $gender = document.querySelectorAll(".radio-button");
   const $textData = document.querySelectorAll(".text-box");
   const $resultNumber = document.querySelector(".result-number");
-  let imc = $textData[1].value / Math.pow($textData[2].value, 2);
+  let imc = Math.round($textData[1].value / Math.pow($textData[2].value, 2), 2);
   let idealWeight = [
-    Math.pow($textData[2].value, 2) * 18.5,
-    Math.pow($textData[2].value, 2) * 24.9,
+    Math.round(Math.pow($textData[2].value, 2) * 18.5, 2),
+    Math.round(Math.pow($textData[2].value, 2) * 24.9, 2),
   ];
-  console.log(idealWeight);
-  $resultNumber.innerHTML = imc;
+  let $genderSelected;
+  $gender.forEach((i) => {
+    if (i.checked) {
+      $genderSelected = i;
+    }
+  });
+  if (imc && imc !== Infinity) {
+    $resultNumber.innerHTML = imc;
+    if ($genderSelected) {
+      // gender
+      if ($textData[0].value) {
+        // age
+        const imcObject = new imcEntry(
+          $genderSelected.id,
+          $textData[0].value,
+          $textData[1].value,
+          $textData[2].value,
+          imc,
+          idealWeight
+        );
+        const savedData = JSON.parse(localStorage.getItem("dataframe"));
+        if (savedData) {
+          savedData.push(imcObject);
+          localStorage.setItem("dataframe", JSON.stringify(savedData));
+        } else {
+          localStorage.setItem("dataframe", JSON.stringify([imcObject]));
+        }
+        resetForm();
+      }
+    }
+  } else {
+    let errorMessage = "Porfavor ingrese su estatura y su peso";
+    $resultNumber.innerHTML = errorMessage;
+  }
 };
 
 const $sendButton = document.querySelector(".send-button");
